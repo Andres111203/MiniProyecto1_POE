31eecd2a3a6c4ab36a5feccb5f4a7a29214b71a7
Andres111203 <andres.canas@correounivalle.edu.co>
2024-03-28 22:34:09 -0500

    este es el primer commit, donde estan las clases que se van a utiilizar en el proyecto

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        int opc = 0;
+        Gestionamiento_Cliente gest_Cliente = new Gestionamiento_Cliente();
+        Scanner scanner = new Scanner(System.in);
+    
+        do{
+    
+
+            
+    
+            
+    
+    
+    
+    
+            }while(opc != 0);
+        }
+
+}
