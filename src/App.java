0e95e68e5137a7af3dbe47ffa94c4fddc11aa0c4
Alejandro Garzon Mayorga <alejand201@gmail.com>
2024-03-30 15:05:57 -0500

    Juan_E

--- src/App.java
+++ src/App.java
@@ -1,13 +1,35 @@
-import javax.swing.JOptionPane;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        int opc = 0;
+        int opc;
         Gestionamiento_Cliente gest_Cliente = new Gestionamiento_Cliente();
         Scanner scanner = new Scanner(System.in);
     
-        do{
-    
+        do
+        {
+            System.out.println("--------Menu de opciones--------");
+            System.out.println("1. Ingredar Clientes");
+            System.out.println("2. Insertar dinero ahorrado");
+            System.out.println("3. Actualizar dinero ahorrado");
+            System.out.println("0. Salir del programa");
+
+            System.out.println("\nSeleccione una oppcion: ");
+            opc = scanner.nextInt();
+
+            switch(opc)
+            {
+                case 1: gest_Cliente.insertarCliente();
+                    break;
+                
+                case 2: gest_Cliente.insertarDineroAhorrado();
+                    break;
+                
+                case 3: gest_Cliente.actualizarDineroAhorrado();
+            }
+        }
+        while(opc != 0);
+        scanner.close();
+        
 
             
     
@@ -16,7 +38,7 @@ public class App {
     
     
     
-            }while(opc != 0);
-        }
+            
+    }
 
 }
--- src/Cliente.java
+++ src/Cliente.java
@@ -1,6 +1,54 @@
-public class Cliente {
+public class Cliente
+{
 
-    
+    private String nombre;
+    private String cedula;
+    private String fecha_Creacion;
+    private double nivel_Ingresos;
+    private double dinero_Ahorrado;
+   
+    public Cliente(String nombre, String cedula, String fecha_Creacion, double nivel_Ingresos, double dinero_Ahorrado) 
+    {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.nivel_Ingresos = nivel_Ingresos;
+        this.fecha_Creacion = fecha_Creacion;
+        this.dinero_Ahorrado = dinero_Ahorrado;
+    }
+    public Cliente()
+    {
+
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public double getDinero_Ahorrado() {
+        return dinero_Ahorrado;
+    }
+    public void setDinero_Ahorrado(double dinero_Ahorrado) {
+        this.dinero_Ahorrado = dinero_Ahorrado;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCedula() {
+        return cedula;
+    }
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    public double getNivel_Ingresos() {
+        return nivel_Ingresos;
+    }
+    public void setNivel_Ingresos(double nivel_Ingresos) {
+        this.nivel_Ingresos = nivel_Ingresos;
+    }
+    public String getFecha_Creacion() {
+        return fecha_Creacion;
+    }
+    public void setFecha_Creacion(String fecha_Creacion) {
+        this.fecha_Creacion = fecha_Creacion;
+    }
 
 
 }
